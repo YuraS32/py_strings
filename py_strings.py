4c6d2c9cdd7672c69159a1860cc844adc2e57ecb
YuraS32 <kre.shaggy@gmail.com>
2024-04-04 10:18:37 +0200

    add hw with pytest str

--- py_strings.py
+++ py_strings.py
@@ -1,83 +1,43 @@
 def reverse(text: str) -> str:
-    """
-    Return the 'text' backwards.
-
-    Parameters
-    ----------
-    text: str
-        The input string
-
-    Returns
-    -------
-    str
-        The text written backwards.
-    """
+    return (text[::-1])
     pass
 
 
 def first_to_upper(text: str) -> str:
-    """
-    Changes each first character of the word to uppercase.
-
-    Parameters
-    ----------
-    text: str
-        The input string
-
-    Returns
-    -------
-    str
-        The modified text
-    """
+    l=text.split()
+    l1=[]
+    for i in l:
+        l1.append(i[0].capitalize()+i[1:])
+    s=" ".join(l1)
+    return(s)
     pass
 
 
 def count_vowels(text: str) -> int:
-    """
-    Counts number of vovels in the text.
-
-    Parameters
-    ----------
-    text: str
-        The input string
-
-    Returns
-    -------
-    inp
-        Number of vowels.
-    """
+    s=list(text)
+    k=0
+    for i in s:
+        if (i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='y' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U' or i=='Y'):
+            k+=1
+    return(k)
     pass
 
 
 def sum_digits(text: str) -> int:
-    """
-    Finds all digitts in the text and returns its sum.
-
-    Parameters
-    ----------
-    text: str
-        The input string
-
-    Returns
-    -------
-    int
-        Sum of all digits in the text.
-    """
+    s = list(text)
+    k = 0
+    for i in s:
+        if (i=='1' or i=='2' or i=='3' or i=='4' or i=='5' or i=='6' or i=='7' or i=='8' or i=='9'):
+            k += int(i)
+    return (k)
     pass
 
 
 def search_substr(text: str, sub: str) -> int:
-    """
-    Search for sub(string) in the text. Returns the position or None.
+    n=len(text)
+    m=len(sub)
+    for i in range(n - m + 1):
+        if text[i: i + m] == sub:
+            return (i)
 
-    Parameters
-    ----------
-    text: str
-        The input string
-
-    Returns
-    -------
-    int or None
-        Position of the sub(string) or None.
-    """
     pass
---
+++ test.py
@@ -0,0 +1,14 @@
+
+
+from py_strings import *
+print(reverse("abc"))
+print(first_to_upper("litwo oJCZYZNO MoJa"))
+print(count_vowels("ABCEXWOOOK"))
+print(sum_digits("It's 911, what is your emergency? 3... 2... 1... kaboom!"))
+print(search_substr("Litwo, Ojczyzno moja", "wa"))
+
+
+
+
+
+
